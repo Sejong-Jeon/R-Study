3d7009b213b9310e99fc6e3319324bc783e3d788
Sejong Jeon <sejong9193@gmail.com>
2021-01-22 16:28:56 +0900

    Add is Function.R

---
+++ is Function.R
@@ -0,0 +1,17 @@
+
+#R data type
+x1 = 1
+x2 = "I Can do it"
+x3 = FALSE
+x4 = 3.14
+#Check Data(True or False)
+is.numeric(x1)
+is.integer(x1)
+is.double(x1)
+is.character(x2)
+is.logical(x2)
+is.complex(x2)
+is.null(x3)
+is.na(x3)
+is.finite(x4)
+is.infinite(x4)
