9dd005835b93512c8a99a161e01852c9bbce0e76
Sejong Jeon <sejong9193@gmail.com>
2021-01-24 23:08:10 +0900

    Add c Function.R

---
+++ c Function.R
@@ -0,0 +1,14 @@
+# Use c Function
+x1 = c(1, 5, 10)
+x2 = c("조조", "관우", "조자룡")
+x3 = c(TRUE, FALSE)
+x4 = c(2, 6, 9)
+
+# vector combination
+x5 = c(x1, x4)
+x5
+
+# Invalid example
+# x6 = (1, 3, 5)
+# x7 = (2, 4, 6)
+# x8 = (x6, x7)
