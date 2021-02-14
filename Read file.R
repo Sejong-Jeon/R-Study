3fcde60bd8d3c33fc4993fe11554d9c9d1d4bda6
Sejong Jeon <sejong9193@gmail.com>
2021-02-14 23:31:45 +0900

    Add Read file.R(Incomplete)

--- Read file.R
+++ Read file.R
@@ -1,10 +1,19 @@
 # Read a space-separated file
 # No such file or directory
 #setwd("C:\\Users\\sejon\\R-Study")
-test=read.table(file="test.txt",
+file1=read.table(file="space-separated file.txt",
                 header=TRUE,
                 sep=" ")
-test
-test$name
-test$x1
-test$x2
+file1
+file1$name
+file1$x1
+file1$x2
+
+# Read a comma-separated file
+file2=read.table(file="comma-separated file",
+                header=TRUE,
+                sep=",")
+file2
+file2$name
+file2$x1
+file2$x2
